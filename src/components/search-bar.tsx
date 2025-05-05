67b3757e1b82628ee1f240f24c2caa30ce36ae90
Bruno Galvão <bgalvaomiranda302@gmail.com>
2025-05-05 08:56:58 -0300

    feat(search-bar): adjust placeholder text

--- src/components/search-bar.tsx
+++ src/components/search-bar.tsx
@@ -25,11 +25,11 @@ export function SearchBar({ onSearch }: SearchBarProps) {
     <div className="relative mx-auto">
       <input
         type="text"
-        placeholder="Digite um usuário do GitHub"
+        placeholder="Digite um usuário"
         value={username}
         onChange={(e) => setUsername(e.target.value)}
         onKeyDown={handleKeyDown}
-        className="w-full pl-4 pr-[4.875rem] py-[1.125rem] text-xl font-semibold placeholder-black text-black bg-white border border-gray-200 rounded-[.625rem] focus:outline-none focus:ring-2 focus:ring-blue"
+        className="w-full pl-3 pr-17 py-[1.125rem] text-base font-semibold placeholder-black text-black bg-white border border-gray-200 rounded-[.625rem] focus:outline-none focus:ring-2 focus:ring-blue md:pl-4 md:pr-[4.875rem] md:text-xl"
       />
       <button
         onClick={handleSearch}
